807df8bd582badcf401f55e636811cdb550559bd
DhanushRajGP09 <dhanush.raj@robosoftin.com>
2023-04-06 16:38:39 +0530

    completed the design of language page

--- src/Components/Languages/Languages.jsx
+++ src/Components/Languages/Languages.jsx
@@ -1,9 +1,162 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
+import "./Languages.css";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addbodyLineNumber,
+  getBodyLineNumber,
+} from "../../features/question/QuestionSlice";
 
 export default function Languages() {
+  const getbodynumbers = useSelector(getBodyLineNumber);
+  console.log("hi", getbodynumbers);
+
+  const dispatch = useDispatch();
+
+  var lineNumber = 2;
+
+  console.log("hehe");
+
+  const EnterFunction = useCallback((event) => {
+    if (event.key === "Enter") {
+      lineNumber = lineNumber + 1;
+      console.log("working", lineNumber + 1);
+
+      dispatch(addbodyLineNumber(lineNumber));
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", EnterFunction, false);
+
+    return () => {
+      document.removeEventListener("keydown", EnterFunction, false);
+    };
+  }, [EnterFunction]);
+
   return (
-    <div>
-      <h1>Languages</h1>
+    <div className="languages">
+      <span className="problemNameText">Allowed languages</span>
+      <span className="maxScoreDescription" style={{ marginTop: "2%" }}>
+        Programming languages in which candidates can write their code.
+      </span>
+      <div className="selectAllLang">
+        <FormGroup>
+          <FormControlLabel control={<Checkbox />} label="Select all" />
+        </FormGroup>
+      </div>
+      <div className="languagesSelectContainer">
+        <div className="languageSelectDiv">
+          {" "}
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="Select all" />
+          </FormGroup>
+        </div>
+        <div className="languageSelectDiv">
+          {" "}
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="Select all" />
+          </FormGroup>
+        </div>
+        <div className="languageSelectDiv">
+          {" "}
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="Select all" />
+          </FormGroup>
+        </div>
+        <div className="languageSelectDiv">
+          {" "}
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="Select all" />
+          </FormGroup>
+        </div>
+        <div className="languageSelectDiv">
+          {" "}
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="Select all" />
+          </FormGroup>
+        </div>
+        <div className="languageSelectDiv">
+          {" "}
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="Select all" />
+          </FormGroup>
+        </div>
+        <div className="languageSelectDiv">
+          {" "}
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="Select all" />
+          </FormGroup>
+        </div>
+        <div className="languageSelectDiv">
+          {" "}
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="Select all" />
+          </FormGroup>
+        </div>
+        <div className="languageSelectDiv">
+          {" "}
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="Select all" />
+          </FormGroup>
+        </div>
+      </div>
+      <span className="problemNameText" style={{ marginTop: "2%" }}>
+        Code snippets
+      </span>
+      <div className="codeSnippetContainer">
+        <div className="codeSnippetContainerHeader">
+          <span className="fullscreenMode">Fullscreen mode</span>
+        </div>
+        <div className="codeSnippetContainerBody">
+          <div className="codeSnippetSelectedLanguages">
+            <div className="codeSnippetSelectedLanguage">C</div>
+            <div className="codeSnippetSelectedLanguage">C++</div>
+            <div className="codeSnippetSelectedLanguage">Java</div>
+            <div className="codeSnippetSelectedLanguage">Python</div>
+            <div className="codeSnippetSelectedLanguage">JavaScript</div>
+          </div>
+          <div className="codeContainerDiv">
+            <div className="codeContainerDivHeader">
+              <div className="codeContainerDivHeaderHeader">
+                <span style={{ color: "white", marginLeft: "2%" }}>Head</span>
+                <span
+                  className="maxScoreDescription"
+                  style={{ marginLeft: "2%", color: "white" }}
+                >
+                  This cannot be edited by the candidate
+                </span>
+              </div>
+            </div>
+            <div className="codeContainerDivBody">
+              <div className="codeContainerDivBodyHeader">
+                <span style={{ color: "white", marginLeft: "2%" }}>Body</span>
+              </div>
+              <div className="codeContainerDivBodyBody">
+                <div className="codeContainerDivBodyBodyNumber">
+                  {getbodynumbers.map((data, index) => {
+                    return <div className="bodyLineNumber">{data}</div>;
+                  })}
+                </div>
+                <textarea className="codeContainerDivBodyTextArea"></textarea>
+              </div>
+            </div>
+            <div className="codeContainerDivHeader">
+              <div className="codeContainerDivHeaderHeader">
+                <span style={{ color: "white", marginLeft: "2%" }}>Tail</span>
+                <span
+                  className="maxScoreDescription"
+                  style={{ marginLeft: "2%", color: "white" }}
+                >
+                  This cannot be edited by the candidate
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/features/question/QuestionSlice.jsx
+++ src/features/question/QuestionSlice.jsx
@@ -5,6 +5,7 @@ const QuestionSlice = createSlice({
   initialState: {
     ProblemName: "",
     ProblemDescription: "",
+    bodyLineNumber: [2],
   },
   reducers: {
     addProblemName: (state, { payload }) => {
@@ -13,13 +14,25 @@ const QuestionSlice = createSlice({
     addProblemDescription: (state, { payload }) => {
       state.ProblemDescription = payload;
     },
+    addbodyLineNumber: (state, { payload }) => {
+      state.bodyLineNumber.push(payload);
+    },
+    removebodyLineNumber: (state, { payload }) => {
+      state.bodyLineNumber.pop();
+    },
   },
 });
 
-export const { addProblemName, addProblemDescription } = QuestionSlice.actions;
+export const {
+  addProblemName,
+  addProblemDescription,
+  addbodyLineNumber,
+  removebodyLineNumber,
+} = QuestionSlice.actions;
 export const getProblemName = (state) => state.Problem.ProblemName;
 
 export const getProblemDescription = (state) =>
   state.Problem.ProblemDescription;
+export const getBodyLineNumber = (state) => state.Problem.bodyLineNumber;
 
 export default QuestionSlice.reducer;
